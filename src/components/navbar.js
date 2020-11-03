f7f6807d437e9a5c6f59c659609e5a900c207b5a
Yazan Deek <ydeek91@yahoo.com>
2020-11-03 13:29:48 -0500

    navbar code is wrrited

---
+++ src/components/navbar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { Link } from "react-router-dom";
+
+
+export default class Example extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.toggleNavbar = this.toggleNavbar.bind(this);
+        this.state = {
+            collapsed: true
+        };
+    }
+
+    toggleNavbar() {
+        this.setState({
+            collapsed: !this.state.collapsed
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Navbar color="faded" light>
+                    <NavbarBrand href="/" className="mr-auto">Sean Belverstone</NavbarBrand>
+                    <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+                    <Collapse isOpen={!this.state.collapsed} navbar>
+                        <Nav navbar>
+                            <NavItem>
+                                <NavLink tag={Link} to="/">Home</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink tag={Link} to="/projects">Projects</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink tag={Link} to="/contact">Contact</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
+}
