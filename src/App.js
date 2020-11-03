6ca776eab83e99cde917bcc56efaa7191ead08f3
Yazan Deek <ydeek91@yahoo.com>
2020-11-03 13:45:25 -0500

    added routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ReactGA from 'react-ga';
+import Navbar from "./components/navbar";
+import MainSection from "./components/about";
+import Projects from "./components/projects";
+import Contact from "./components/contact";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUser, faEnvelopeOpenText, faPhone, faCommentAlt } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
+library.add(faUser, faEnvelopeOpenText, faPhone, faCommentAlt);
+
+ReactGA.initialize("UA-153622944-1");
+ReactGA.pageview(window.location.pathname + window.location.search);
+
+
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+      <div>
+        <Navbar />
+
+        <Route exact path="/" component={MainSection} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/contact" component={Contact} />
+
+      </div>
+    </Router>
   );
 }
+
 
 export default App;
